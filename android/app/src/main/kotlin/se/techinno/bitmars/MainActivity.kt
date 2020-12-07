b459b2a9588e977e5a78b8b9833321893b7533bb
Vibhakar Sarswat <vibhakar.sarswat@productmadness.com>
2020-12-07 05:14:59 +0000

    Added initial project with 'main.dart', 'price_screen.dart' and 'coin_data.dart' files.

---
+++ android/app/src/main/kotlin/se/techinno/bitmars/MainActivity.kt
@@ -0,0 +1,6 @@
+package se.techinno.bitmars
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:bitmars/price_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(primaryColor: Colors.lightBlue, scaffoldBackgroundColor: Colors.white),
+      home: PriceScreen(),
+    );
+  }
+}
